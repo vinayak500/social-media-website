3f3af5feaa0e377c89af2605b7265c40ae9706aa
vinayak500 <vinayakshetty500@gmail.com>
2022-09-16 16:24:38 +0530

    added async await feature for create , delete post and displaying in home page

--- controller/homecontroller.js
+++ controller/homecontroller.js
@@ -1,23 +1,50 @@
 
 const post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = function(req , res){
+module.exports.home = async function(req , res){
 
     // populate the user of each part
-    post.find({}).populate('user').populate({
-        path: 'comments',
-        populate: {
-                path:'user'
-        }
-       }
-       ).exec(function(err,posts){
-        User.find({} , function(err , users){
-            return res.render('home' , {
-                    title:'Codeial | Home' ,
-                    posts : posts ,
-                    all_users : users 
-          });
-     })
-});
+//     post.find({}).populate('user').populate({
+//         path: 'comments',
+//         populate: {
+//                 path:'user'
+//         }
+//        }
+//        ).exec(function(err,posts){
+//         User.find({} , function(err , users){
+//             return res.render('home' , {
+//                     title:'Codeial | Home' ,
+//                     posts : posts ,
+//                     all_users : users 
+//           });
+//      })
+// });
+
+
+try{
+    let posts = await post.find({}).populate('user').populate({
+      path: 'comments',
+      populate: {
+              path:'user'
+      }
+     });
+
+     let users = await User.find({});
+
+
+     return res.render('home' , {
+      title:'Codeial | Home' ,
+      posts : posts ,
+      all_users : users 
+       });
+      }    
+catch(err) {
+      console.log(err);
+      }
+
+
+
+
+
 }; 
 
--- controller/postcontroller.js
+++ controller/postcontroller.js
@@ -1,29 +1,63 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
-module.exports.create = function(req , res){
-    Post.create({
-        content: req.body.content ,
-        user : req.user._id
-    } , function(err, post){
-        if(err){console.log('error in creating a post'); return; }
+module.exports.create = async function(req , res){
+    // Post.create({
+    //     content: req.body.content ,
+    //     user : req.user._id
+    // } , function(err, post){
+    //     if(err){console.log('error in creating a post'); return; }
+    //     return res.redirect('back');
+    // })
+
+
+
+    try{
+        await Post.create({
+            content: req.body.content ,
+            user : req.user._id
+        });
+
         return res.redirect('back');
-    })
+    }
+    catch(err){
+        console.log('Error' + err);
+    }
 }
 
-module.exports.destroy = function(req , res){
-    Post.findById(req.params.id , function(err , post){
+module.exports.destroy = async function(req , res){
+    // Post.findById(req.params.id , function(err , post){
         // post.user is the id , 
         // .id means converting object is to string 
-        console.log(post.user);
-        console.log(req.user.id);
-        if(post.user == req.user.id){
-               post.remove();
-               Comment.deleteMany({post : req.params.id} , function(err){
-                       return res.redirect('back');
-               });
+    //     console.log(post.user);
+    //     console.log(req.user.id);
+    //     if(post.user == req.user.id){
+    //            post.remove();
+    //            Comment.deleteMany({post : req.params.id} , function(err){
+    //                    return res.redirect('back');
+    //            });
 
-        } else{
-            return res.redirect('back');
-        }
-    })
+    //     } else{
+    //         return res.redirect('back');
+    //     }
+    // })
+
+
+
+
+
+    try{
+        let  posts = await Post.findById(req.params.id);
+        if(posts.user == req.user.id){
+            posts.remove();
+           await  Comment.deleteMany({post : req.params.id});
+                    return res.redirect('back');
+                } else{
+                    return res.redirect('back');
+                }
+               }
+               catch(err){
+                   console.log('Error' + err);
+               }
+
+
 }
